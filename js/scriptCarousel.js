7a63669deb73f4f0f5e047b8bcefc4abbfeb5ba9
gurnav <singh.g@outlook.fr>
2017-07-02 21:12:05 +0200

    Animate title and description display differently done

--- js/scriptCarousel.js
+++ js/scriptCarousel.js
@@ -1,18 +1,18 @@
-/*
-Afficher un message si le JS n'est pas activé (done)
-Appel des données en AJAX (format JSON) https://www.skrzypczyk.fr/slideshow.php (done)
-
-Fonctionnalités :
-  Animation de l'affichage des titres et descriptions diff de l'animation des images (not done)
-  Rognage des images ou autre solution (done)
-  Suivant, précédent, play, pause (done)
-  KeyBoard Suivant, précédent, play, pause (done)
-  Anti spam-click (done)
-  Hover Play pause (done)
-  Responsive et BEAU (halfly-done)
-  Administrable via une liste de variable en haut de script (done)
-  Module de controle : Puces ou vignettes (not done)
-*/
+/**
+ * Afficher un message si le JS n'est pas activé (done)
+ * Appel des données en AJAX (format JSON) https://www.skrzypczyk.fr/slideshow.php (done)
+ *
+ * Fonctionnalités :
+ * Animation de l'affichage des titres et descriptions diff de l'animation des images (done)
+ * Rognage des images ou autre solution (done)
+ * Suivant, précédent, play, pause (done)
+ * KeyBoard Suivant, précédent, play, pause (done)
+ * Anti spam-click (done)
+ * Hover Play pause (done)
+ * Responsive et BEAU (halfly-done)
+ * Administrable via une liste de variable en haut de script (done)
+ * Module de controle : Puces ou vignettes (not done)
+ */
 
 
 $(document).ready(function () {
@@ -25,7 +25,9 @@ $(document).ready(function () {
 
   preloadSlide(slides);
 
-  // slideshow functions referred each to an id that calls each buttons of our slider
+  /**
+   * Slideshow functions referred each to an id that calls each buttons of our slider
+   */
   $('#next').click(fonctionNext);
   $('#previous').click(fonctionPrevious);
   $('#playOrStop').click(fonctionPlayStop);
@@ -47,7 +49,10 @@ $(document).ready(function () {
     return data;
   }
 
-  //function to display and load all the pictures, title, description
+  /**
+   * Function to display and load all the pictures, title, description
+   * @param slides The JSON array of slides 
+   */
   function preloadSlide (slides) {
     let slideSize = slides.length * $(this).width();
     slides.forEach(function (element) {
@@ -57,14 +62,18 @@ $(document).ready(function () {
     $('#slide').css('width', slideSize);
   }
 
-  //function to display the next picture of it's actual
+  /**
+   * Function to display the next picture of it's actual
+   */
   function fonctionNext () {
     // Stop the function if we're already sliding
-    if(sliding){
-        return false;
-    }
+    if (sliding) { return false; }
     // And now we're sliding
     sliding = true;
+    // Hiding the text of the next slide for displaying purposes
+    $('#slide .element:nth-child(2) .divTitle').hide();
+    $('#slide .element:nth-child(2) .divDesc').hide();
+    // Sliding animation
     $('#slide').animate(
       { marginLeft: '-700px' },
       speed,
@@ -73,18 +82,25 @@ $(document).ready(function () {
         $('#slide').css('margin-left', '0px');
         // Slide over!
         sliding = false;
+        // When sliding done display the text smoothly
+        $('#slide .element:first .divTitle').fadeIn('slow');
+        $('#slide .element:first .divDesc').fadeIn(3000);
       }
     );
   }
 
-  //function to display the previous picture of it's actual
+  /**
+   * Function to display the previous picture of it's actual
+   */
   function fonctionPrevious () {
     // Stop the function if we're already sliding
-    if(sliding){
-        return false;
-    }
+    if (sliding) { return false; }
     // And now we're sliding
     sliding = true;
+    // Hiding the text of the last slide for displaying purposes
+    $('#slide .element:last .divTitle').hide();
+    $('#slide .element:last .divDesc').hide();
+    // Sliding animation
     $('#slide').animate(
       { marginLeft: '0px' },
       speed,
@@ -93,12 +109,17 @@ $(document).ready(function () {
         $('#slide').css('margin-left', '-700px');
         // Slide over!
         sliding = false;
+        // When sliding done display the text smoothly
+        $('#slide .element:first .divTitle').fadeIn('slow');
+        $('#slide .element:first .divDesc').fadeIn(3000);
       }
     );
   }
 
-  //this function set a timer that played with the play/pause function of the slider 
-  //if the user click on the play button or press on spacebar it'll stop the slider
+  /**
+   * This function set a timer that played with the play/pause function of the slider
+   * If the user click on the play button or press on spacebar it'll stop the slider
+   */ 
   function fonctionPlayStop () {
     let action = $('#playOrStop').attr('action');
 
@@ -116,52 +137,48 @@ $(document).ready(function () {
     }
   }
 
-  // Fade in-out function not totally correct 
-  /*$('#slide').hover(function () {
-      $('#nav').fadeIn('slow');
-    }, function () {
-        $('#nav').fadeOut('slow');
-      }
-  );*/
+  /** Fade in-out function not totally correct 
+   * $('#slide').hover(function () {
+   *     $('#nav').fadeIn('slow');
+   * }, function () {
+   *     $('#nav').fadeOut('slow');
+   *    }
+   * );
+   */
 
-  // Fade in-out of button Play/Stop when Mouse enter or leaves
-  $("#slide").hover(function() {
+  /**
+   * Fade in-out of button Play/Stop when Mouse enter or leaves
+   */
+  $("#slide").hover( function() {
     if (!play && hover) {
       clearInterval(play);
       console.log("in : " + play);
     }
-  }, function() {
+  }, function () {
     if (!play && hover) {
       play = setInterval(fonctionNext, speed);
       console.log("out : " + play);
-    } else if (!play && !hover) {
-      hover = true;
-    }
+    } else if (!play && !hover) { hover = true; }
   });
 
 
-  //function with keyboard next/previous action with directional touch (<>) 
-  //and play/stop action with the space key
+  /**
+   * Function with keyboard next/previous action with directional touch (<>) 
+   * and play/stop action with the space key
+   */ 
   $('body').keyup(function (e) {
-    if (e.keyCode === 37) {
-      fonctionPrevious();
-    }
-    if (e.keyCode === 39) {
-      fonctionNext();
-    }
-    if (e.keyCode === 32) {
-      fonctionPlayStop();
-    }
+    if (e.keyCode === 37) { fonctionPrevious(); }
+    if (e.keyCode === 39) { fonctionNext(); }
+    if (e.keyCode === 32) { fonctionPlayStop(); }
   });
 
 
-  //function dots navigations 
-  $('ul li').bind('click', function(){
-
-        //var slide = $(this).slide() + 1;
-
+  /**
+   * Function dots navigations
+   */
+  $('ul li').bind('click', function() {
+        // let slide = $(this).slide() + 1;
         $(".active").fadeOut(300);
-
         $("#slide").fadeIn(300);        
         $("#slide").removeClass("active");
         $("#slide").addClass("active");
